4df182f4fd19a3a91b9e56934cad28def7a2096b
danymat <d.danymat@gmail.com>
2022-12-30 21:44:44 +0000

    feat: add types

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,6 +1,33 @@
 use std::fs;
 
+enum Hand {
+    Rock,
+    Paper,
+    Scissor,
+}
+
+enum Game {
+    Win,
+    Lose,
+    Draw,
+}
+
 fn main() {
-    let file = fs::read_to_string("../input.txt").expect("Error getting input");
-    println!("{}", file);
+    // let file = fs::read_to_string("../input.txt").expect("Error getting input");
+
+    // TODO: calculate scores
+    // TODO: add clippy rust 
+    
+    let game: (Hand, Hand) = (Hand::Rock, Hand::Scissor);
+    match game {
+        (Hand::Rock, Hand::Rock) => Game::Draw,
+        (Hand::Rock, Hand::Paper) => Game::Lose,
+        (Hand::Rock, Hand::Scissor) => todo!(),
+        (Hand::Paper, Hand::Rock) => todo!(),
+        (Hand::Paper, Hand::Paper) => todo!(),
+        (Hand::Paper, Hand::Scissor) => todo!(),
+        (Hand::Scissor, Hand::Rock) => todo!(),
+        (Hand::Scissor, Hand::Paper) => todo!(),
+        (Hand::Scissor, Hand::Scissor) => todo!(),
+    }
 }
