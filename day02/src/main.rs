d8aab20c12043a06a27525258db30bfb32062666
danymat <d.danymat@gmail.com>
2022-12-31 15:15:56 +0000

    feat: Add day2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,33 +1,30 @@
 use std::fs;
 
-enum Hand {
-    Rock,
-    Paper,
-    Scissor,
-}
-
-enum Game {
-    Win,
-    Lose,
-    Draw,
+fn calculate_score(game: &str) -> i32 {
+    let (opponent, yours): (i32, i32) = match game {
+        "A X" => (1, 1),
+        "A Y" => (1, 2),
+        "A Z" => (1, 3),
+        "B X" => (2, 1),
+        "B Y" => (2, 2),
+        "B Z" => (2, 3),
+        "C X" => (3, 1),
+        "C Y" => (3, 2),
+        "C Z" => (3, 3),
+        _ => (0, 0),
+    };
+    let mut score = 0;
+    score += yours;
+    if opponent == yours {
+        score += 3;
+    } else if (yours - opponent) == 1 || (yours == 1 && opponent == 3) {
+        score += 6;
+    }
+    score
 }
 
 fn main() {
-    // let file = fs::read_to_string("../input.txt").expect("Error getting input");
-
-    // TODO: calculate scores
-    // TODO: add clippy rust 
-    
-    let game: (Hand, Hand) = (Hand::Rock, Hand::Scissor);
-    match game {
-        (Hand::Rock, Hand::Rock) => Game::Draw,
-        (Hand::Rock, Hand::Paper) => Game::Lose,
-        (Hand::Rock, Hand::Scissor) => todo!(),
-        (Hand::Paper, Hand::Rock) => todo!(),
-        (Hand::Paper, Hand::Paper) => todo!(),
-        (Hand::Paper, Hand::Scissor) => todo!(),
-        (Hand::Scissor, Hand::Rock) => todo!(),
-        (Hand::Scissor, Hand::Paper) => todo!(),
-        (Hand::Scissor, Hand::Scissor) => todo!(),
-    }
+    let file = fs::read_to_string("../input.txt").expect("Error getting input");
+    let score: i32 = file.lines().map(|line| calculate_score(line)).sum();
+    println!("{}", score);
 }
